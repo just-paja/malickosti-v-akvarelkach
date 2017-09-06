350fe8664537a665aaa99e38c53df9be907928ad
just-paja <just.paja@gmail.com>
2017-09-06 20:33:57 +0200

    Reserved state

--- web/views/drawings.py
+++ web/views/drawings.py
@@ -3,6 +3,7 @@ from django.http import Http404
 
 from ..models import (
     Drawing,
+    DRAWING_AVAILABLE_STATES,
     DRAWING_STATUS_STORED,
     DRAWING_STATUS_RESERVED,
     DRAWING_STATUS_SOLD,
@@ -36,4 +37,6 @@ def view_drawings_detail(request, id):
     return render(request, 'drawings-detail.html', {
         'drawing': drawing,
         'price': drawing.get_price(),
+        'available': drawing.status in DRAWING_AVAILABLE_STATES,
+        'status_text': drawing.get_status_display,
     })
