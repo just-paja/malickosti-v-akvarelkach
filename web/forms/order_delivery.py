afa53fdef3d507b3cadd99a204d72b3d6da894b0
Pavel Žák <pavel@berrycloud.cz>
2017-09-11 09:11:20 +0200

    Select delivery and payment method

--- web/forms/order_delivery.py
+++ web/forms/order_delivery.py
@@ -1,6 +1,53 @@
-from django import forms
+from django.forms import Form, IntegerField
+from django.core.exceptions import (
+    ObjectDoesNotExist,
+    ValidationError,
+)
+from django.utils.translation import ugettext_lazy as _
+
+from ..models import (
+    DeliveryMethod,
+    PaymentMethod,
+)
+
+def validate_id(value, model):
+    try:
+        model.objects.get(id=value)
+    except ObjectDoesNotExist:
+        raise ValidationError(_('Invalid Method'), params={
+            'value': value,
+        })
+
+def validate_delivery_method(value):
+    return validate_id(value, DeliveryMethod)
+
+def validate_payment_method(value):
+    return validate_id(value, PaymentMethod)
 
 
-class OrderDelivery(forms.Form):
-    delivery_method = forms.IntegerField()
-    payment_method = forms.IntegerField()
+class DeliveryMethodField(IntegerField):
+    default_validators = [validate_delivery_method]
+
+
+class PaymentMethodField(IntegerField):
+    default_validators = [validate_payment_method]
+
+
+class OrderDelivery(Form):
+    delivery_method = DeliveryMethodField()
+    payment_method = PaymentMethodField()
+
+    def clean(self):
+        cleaned_data = super(OrderDelivery, self).clean()
+        delivery_method_value = cleaned_data.get('delivery_method')
+        payment_method_value = cleaned_data.get('payment_method')
+
+        exists = DeliveryMethod.objects.filter(
+            id=delivery_method_value,
+            payment_methods__in=[payment_method_value]
+        ).count()
+
+        if not exists:
+            raise ValidationError(_('Invalid combination of Delivery and Payment methods'))
+
+        return cleaned_data
--- web/views/orders.py
+++ web/views/orders.py
@@ -17,7 +17,9 @@ def view_order_delivery(request):
     if request.method == 'POST':
         formset = OrderDelivery(request.POST)
         if formset.is_valid():
-            print(formset.cleaned_data)
+            order = request.session.get('order', {})
+            order['delivery_method'] = formset.cleaned_data.get('delivery_method')
+            order['payment_method'] = formset.cleaned_data.get('payment_method')
     else:
         formset = OrderDelivery()
 
@@ -28,9 +30,25 @@ def view_order_delivery(request):
     for drawing in drawings:
         price += drawing.get_price()
 
+    delivery_method_default = DeliveryMethod.objects.filter(
+        visibility=VISIBILITY_PUBLIC,
+    ).order_by('weight').first()
+
+    payment_method_default = delivery_method_default.payment_methods.filter(
+        visibility=VISIBILITY_PUBLIC,
+    ).order_by('weight').first()
+
     return render(request, 'order/delivery.html', {
         'added': 'pridano' in request.GET,
         'delivery_methods': DeliveryMethod.objects.filter(visibility=VISIBILITY_PUBLIC),
+        'value_delivery_method': int(formset.data.get(
+            'delivery_method',
+            delivery_method_default.id
+        )),
+        'value_payment_method': int(formset.data.get(
+            'payment_method',
+            payment_method_default.id
+        )),
         'drawings': drawings,
         'empty': len(drawings) == 0,
         'formset': formset,
