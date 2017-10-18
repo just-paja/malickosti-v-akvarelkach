acbe77c08b67a8b1d200e8adbed4b5118efdb6c4
Pavel Žák <pavel@berrycloud.cz>
2017-10-18 09:59:04 +0200

    Add some OpenGraph to templates

--- web/views/conditions.py
+++ web/views/conditions.py
@@ -6,4 +6,5 @@ from ..models import TextConditions
 def view_conditions(request):
     return render(request, 'conditions.html', {
         'conditions': TextConditions.objects.get_visible(),
+        'last_changed': TextConditions.objects.order_by('-modified').first(),
     })
