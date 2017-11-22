549e424e396078c845e895f3b8b94113445b831b
Pavel Žák <pavel@berrycloud.cz>
2017-11-22 13:43:25 +0100

    Hotfix S3 <-> django-imagekit issue

--- malickosti/storages.py
+++ malickosti/storages.py
@@ -1,15 +1,49 @@
 # custom_storages.py
+import os
+from tempfile import SpooledTemporaryFile
+
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class DatabaseBackupStorage(S3Boto3Storage):
+class S3Boto3StorageFixed(S3Boto3Storage):
+    """
+    This is our custom version of S3Boto3Storage that fixes a bug in boto3 where the passed
+    in file is closed upon upload.
+
+    https://github.com/boto/boto3/issues/929
+    https://github.com/matthewwithanm/django-imagekit/issues/391
+    """
+
+    def _save_content(self, obj, content, parameters):
+        """
+        We create a clone of the content file as when this is passed to boto3 it wrongly closes
+        the file upon upload where as the storage backend expects it to still be open
+        """
+        # Seek our content back to the start
+        content.seek(0, os.SEEK_SET)
+
+        # Create a temporary file that will write to disk after a specified size
+        content_autoclose = SpooledTemporaryFile()
+
+        # Write our original content into our copy that will be closed by boto3
+        content_autoclose.write(content.read())
+
+        # Upload the object which will auto close the content_autoclose instance
+        super(S3Boto3StorageFixed, self)._save_content(obj, content_autoclose, parameters)
+
+        # Cleanup if this is fixed upstream our duplicate should always close
+        if not content_autoclose.closed:
+            content_autoclose.close()
+
+
+class DatabaseBackupStorage(S3Boto3StorageFixed):
     location = settings.DATABASE_BACKUP_FILES_LOCATION
 
 
-class MediaStorage(S3Boto3Storage):
+class MediaStorage(S3Boto3StorageFixed):
     location = settings.MEDIA_FILES_LOCATION
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(S3Boto3StorageFixed):
     location = settings.STATIC_FILES_LOCATION
