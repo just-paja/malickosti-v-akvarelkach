f94d243ac6b31fd3adc00e4ff43cc11997abc5b4
just-paja <just.paja@gmail.com>
2017-12-29 00:00:19 +0100

    Add drawings pagination

--- web/views/drawings.py
+++ web/views/drawings.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.http import Http404
@@ -11,8 +13,15 @@ from drawings.models import (
     DRAWING_STATUS_SOLD,
 )
 
+PAGE_SIZE = 20
+
 
 def view_drawings(request):
+    try:
+        page = int(request.GET.get('stranka', 1))
+    except ValueError:
+        raise Http404
+
     for_sale = request.GET.get('prodejne', None)
     size_id = request.GET.get('velikost', None)
     sizes = DrawingSize.objects.all()
@@ -38,10 +47,25 @@ def view_drawings(request):
         query_params['velikost'] = size.id
         drawings = drawings.filter(size=size_id)
 
-    drawings = drawings.order_by('status', 'name', '-created')
+    if page < 1:
+        raise Http404
+
+    start = (page - 1) * PAGE_SIZE
+    end = (page - 1) * PAGE_SIZE + PAGE_SIZE
+    items_total = drawings.count()
+    drawings = drawings.order_by('status', 'name', '-created')[start:end]
+    pages_total = ceil(items_total / PAGE_SIZE)
 
     return render(request, 'drawings/index.html', {
         'drawings': drawings,
+        'items_total': items_total,
+        'items_visible': len(drawings),
+        'page_current': page,
+        'page_next': page + 1,
+        'page_prev': page - 1,
+        'pages_total': pages_total,
+        'pages': range(1, pages_total + 1),
+        'per_page': PAGE_SIZE,
         'query_params': query_params,
         'show_size': show_size,
         'sizes': sizes,
